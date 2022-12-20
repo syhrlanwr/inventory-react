5c20b4d2070d2339dea9df447a0e6c401fd4aa66
Indri Nurfiani <indrinurfiani99@gmail.com>
2022-12-20 19:15:53 +0700

    add rak and satuan edited

--- src/addSatuan.js
+++ src/addSatuan.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const AddSatuan = () => {
+function AddSatuan () {
     const [nama, setNama] = useState('');
     const navigate = useNavigate();
     
