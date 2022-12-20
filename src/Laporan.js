dc624f7eaf749449d5c96f67bd1cdf203a576c2d
syarif2002 <rongodsyarif@gmail.com>
2022-12-20 15:31:21 +0700

    add asa

---
+++ src/Laporan.js
@@ -0,0 +1,12 @@
+<table class="table-auto w-full text-md bg-white shadow-md rounded-lg overflow-hidden">
+  <thead class="bg-gray-800 border-b border-gray-300 text-white">
+    <tr>
+      <th class="text-left px-4 py-2">No</th>
+      <th class="text-left px-4 py-2">Keterangan</th>
+      <th class="text-left px-4 py-2">Jumlah</th>
+      <th class="text-left px-4 py-2">Nama Barang</th>
+      <th class="text-left px-4 py-2">Tanggal</th>
+      <th class="text-left px-4 py-2"></th>
+    </tr>
+  </thead>
+</table>;
