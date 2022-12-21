89cc86986efe43b38545e1a50fb68e29f0fcbd8f
syarif2002 <rongodsyarif@gmail.com>
2022-12-21 14:05:06 +0700

    add pegawai

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import Login from './Login';
-// import AddPegawai from './addPegawai';
-// import EditPegawai from './editPegawai';
-// import KelolaPegawai from './kelolaPegawai';
+import AddPegawai from './AddPegawai';
+import EditPegawai from './EditPegawai';
+import KelolaPegawai from './KelolaPegawai';
 // import AddRak from './addRak';
 import EditRak from './EditRak';
 // import KelolaRak from './kelolaRak';
@@ -13,7 +13,6 @@ import AddUser from './AddUser';
 import EditUser from './EditUser';
 import Layout from './layouts/Layout';
 import Login from './Login';
-import KelolaPegawai from './KelolaPegawai';
 import KelolaRak from './KelolaRak';
 import KelolaSatuan from './KelolaSatuan';
 import AddRak from './AddRak';
@@ -22,30 +21,179 @@ import EditJenis from './EditJenis';
 import KelolaJenis from './KelolaJenis';
 import Home from './Home';
 
-
-
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/users" element={<Layout><KelolaUser /></Layout>} />
-        <Route path="/users/add" element={<Layout><AddUser /></Layout>} />
-        <Route path="/users/edit/:id" element={<Layout><EditUser /></Layout>} />
-        <Route path="/users/delete/:id" element={<Layout><KelolaUser /></Layout>} />
-        <Route path="/pegawai" element={<Layout><KelolaPegawai /></Layout>} />
-        <Route path="/rak" element={<Layout><KelolaRak /></Layout>} />
-        <Route path="/rak/add" element={<Layout><AddRak /></Layout>} />
-        <Route path="/rak/edit/:id" element={<Layout><EditRak /></Layout>} />
-        <Route path="/rak/delete/:id" element={<Layout><KelolaRak /></Layout>} />
-        <Route path="/satuan" element={<Layout><KelolaSatuan /></Layout>} />
-        <Route path="/satuan/add" element={<Layout><AddSatuan /></Layout>} />
-        <Route path="/satuan/edit/:id" element={<Layout><EditSatuan /></Layout>} />
-        <Route path="/satuan/delete/:id" element={<Layout><KelolaSatuan /></Layout>} />
-        <Route path="/jenis" element={<Layout><KelolaJenis /></Layout>} />
-        <Route path="/jenis/add" element={<Layout><AddJenis /></Layout>} />
-        <Route path="/jenis/edit/:id" element={<Layout><EditJenis /></Layout>} />
-        <Route path="/jenis/delete/:id" element={<Layout><KelolaJenis /></Layout>} />
+        <Route
+          path="/users"
+          element={
+            <Layout>
+              <KelolaUser />
+            </Layout>
+          }
+        />
+        <Route
+          path="/users/add"
+          element={
+            <Layout>
+              <AddUser />
+            </Layout>
+          }
+        />
+        <Route
+          path="/users/edit/:id"
+          element={
+            <Layout>
+              <EditUser />
+            </Layout>
+          }
+        />
+        <Route
+          path="/users/delete/:id"
+          element={
+            <Layout>
+              <KelolaUser />
+            </Layout>
+          }
+        />
+        <Route
+          path="/pegawai"
+          element={
+            <Layout>
+              <KelolaPegawai />
+            </Layout>
+          }
+        />
+        <Route
+          path="/rak"
+          element={
+            <Layout>
+              <KelolaRak />
+            </Layout>
+          }
+        />
+        <Route
+          path="/rak/add"
+          element={
+            <Layout>
+              <AddRak />
+            </Layout>
+          }
+        />
+        <Route
+          path="/rak/edit/:id"
+          element={
+            <Layout>
+              <EditRak />
+            </Layout>
+          }
+        />
+        <Route
+          path="/rak/delete/:id"
+          element={
+            <Layout>
+              <KelolaRak />
+            </Layout>
+          }
+        />
+        <Route
+          path="/satuan"
+          element={
+            <Layout>
+              <KelolaSatuan />
+            </Layout>
+          }
+        />
+        <Route
+          path="/satuan/add"
+          element={
+            <Layout>
+              <AddSatuan />
+            </Layout>
+          }
+        />
+        <Route
+          path="/satuan/edit/:id"
+          element={
+            <Layout>
+              <EditSatuan />
+            </Layout>
+          }
+        />
+        <Route
+          path="/satuan/delete/:id"
+          element={
+            <Layout>
+              <KelolaSatuan />
+            </Layout>
+          }
+        />
+        <Route
+          path="/jenis"
+          element={
+            <Layout>
+              <KelolaJenis />
+            </Layout>
+          }
+        />
+        <Route
+          path="/jenis/add"
+          element={
+            <Layout>
+              <AddJenis />
+            </Layout>
+          }
+        />
+        <Route
+          path="/jenis/edit/:id"
+          element={
+            <Layout>
+              <EditJenis />
+            </Layout>
+          }
+        />
+        <Route
+          path="/jenis/delete/:id"
+          element={
+            <Layout>
+              <KelolaJenis />
+            </Layout>
+          }
+        />
+        <Route
+          path="/pegawai"
+          element={
+            <Layout>
+              <KelolaPegawai />
+            </Layout>
+          }
+        />
+        <Route
+          path="/pegawai/add"
+          element={
+            <Layout>
+              <AddPegawai />
+            </Layout>
+          }
+        />
+        <Route
+          path="/pegawai/edit/:id"
+          element={
+            <Layout>
+              <EditPegawai />
+            </Layout>
+          }
+        />
+        <Route
+          path="/pegawai/delete/:id"
+          element={
+            <Layout>
+              <KelolaPegawai />
+            </Layout>
+          }
+        />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
--- src/addPegawai.js
+++ src/addPegawai.js
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function AddPegawai() {
   const [nama, setNama] = useState('');
@@ -7,7 +9,7 @@ function AddPegawai() {
 
   const savePegawai = async (e) => {
     e.preventDefault();
-    await axios.post('http://localhost:3001/pegawai', {
+    await axios.post('http://localhost:5000/pegawai', {
       nama: nama,
       nip: nip,
     });
@@ -15,19 +17,19 @@ function AddPegawai() {
   };
   return (
     <div className="bg- white rounded-lg shadow-lg">
-      <form action="/pegawai/add" method="POST" className="flex flex-col px-8 py -6">
+      <form action="/pegawai/add" method="POST" className="flex flex-colpx-8 py -6 " onSubmit={savePegawai}>
         <div className="px-6 py-4 text-xl">
           <div className="flex flex-col mb-6">
             <label for="nip" className="mb-1 text-gray-600">
               NIP
             </label>
-            <input type="text" name="nip" placeholder="NIP" className="border rounded-lg px-3 py-2 mt-1 focus:outline-none focus:border-blue-500" required="required" />
+            <input type="text" name="nip" placeholder="NIP" className="border rounded-lg px-3 py-2 mt-1 focus:outline-none focus:border-blue-500" onChange={(e) => setNama(e.target.value)} value={nama} />
           </div>
           <div className="flex flex-col mb-6">
             <label for="name" className="mb-1 text-gray-600">
               Nama
             </label>
-            <input type="text" name="nama" placeholder="Nama" className="border rounded-lg px-3 py-2 mt-1 focus:outline-none focus:border-blue-500" required="required" />
+            <input type="text" name="nama" placeholder="Nama" className="border rounded-lg px-3 py-2 mt-1 focus:outline-none focus:border-blue-500" onChange={(e) => setNip(e.target.value)} value={nip} />
           </div>
           <button type="submit" className="bg-sky-500 text-white px-4 py-2 rounded font-medium hover:bg-sky-600">
             Tambahkan
